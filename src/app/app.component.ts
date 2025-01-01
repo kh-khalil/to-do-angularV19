6927a105f7f78f5df5f28be11574a565c63ddff2
Khaled Khalil <Khaled.m.hassan.94@gmail.com>
2025-01-01 10:12:03 +0200

    if the http request returns EMPTY or VOID, success & complete will not be called.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { filter, of } from 'rxjs';
 import { IToDo } from '../data/to-do/to-do.interface';
 import { ToDoStore } from '../signal-store/to-do.store';
 
@@ -7,8 +9,9 @@ import { ToDoStore } from '../signal-store/to-do.store';
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [HttpClient],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-manager';
 
   readonly #toDoStore = inject(ToDoStore);
@@ -20,7 +23,7 @@ export class AppComponent {
   toDos = this.#toDoStore.filteredToDos;
   toDosError = this.#toDoStore.toDosError;
   usersError = this.#toDoStore.usersError;
-
+  http = inject(HttpClient);
   onFilter(ele: EventTarget | null) {
     this.#toDoStore.filterToDos((ele as HTMLInputElement).checked);
   }
@@ -34,4 +37,31 @@ export class AppComponent {
   onChangeStatus(task: IToDo, ele: EventTarget | null) {
     this.#toDoStore.changeStatus(task, (ele as HTMLInputElement).checked);
   }
+
+  //#region if the http request returns EMPTY or VOID, success & complete will not be called.
+  test(isPew: boolean) {
+    return isPew
+      ? of([1, 2, 3, 4, 5])
+      : this.http
+          .get('https://jsonplaceholder.typicode.com/todos/1')
+          // this pipe is to mimic the return of void or EMPTY
+          .pipe(filter((value: any) => value.completed === true));
+  }
+
+  log(value: any) {
+    console.log('a7eeh');
+    console.log('pewpew', value);
+  }
+
+  ngOnInit(): void {
+    // if the http request returns EMPTY, success & complete will not be called.
+    this.test(false).subscribe({
+      next: (value) => {
+        this.log(value);
+      },
+      error: (error) => console.error(error),
+      complete: () => console.log('complete'),
+    });
+  }
+  //#endregion
 }
